ff9e595f0c54a3133d53316544db9faffee76c85
Haber He <Haber.He@morningstar.com>
2016-01-04 13:51:57 +0800

    展示摘要

--- content-list.php
+++ content-list.php
@@ -9,7 +9,8 @@
             <div class="media-body">
               <span class="media-heading"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></span>
               <span class="media-summary hidden-xs">
-              教宗方济各11月6日在圣玛尔大之家的清晨弥撒中表示，主教和司铎要克胜“双重生活”的诱惑，教会蒙召做仆役，而非“生意人”。教宗还告诫道，要警惕“贪恋钱财的趋炎附势者”，因为他们对教会的危害极大。
+                <?php the_excerpt() ?>
+                 <!-- echo mb_strimwidth(strip_tags(apply_filters('the_content', $post->post_content)), 0, 200,"···");  -->
               </span>
               <span class="visible-xs-block"><?php the_time('Y-m-d') ?></span>
             </div>
